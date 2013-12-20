b841fa47bc21920184a356c9a27172788e81bd30
Diego Valerio Camarda <dcamarda@regesta.com>
2013-12-20 19:17:17 +0100

    added multiple versions of text recovery feature (beta)

--- localautosave/langs/pt_BR.js
+++ localautosave/langs/pt_BR.js
@@ -1,6 +1,8 @@
 tinyMCE.addI18n('pt_BR.localautosave',{
 	'localautosave.restoreContent' : 'Restaurar Conteúdo',
 	'localautosave.chooseVersion' : 'Choose what you want to restore',
+	'localautosave.chars' : 'chars',
+	'localautosave.clearAll' : 'Discard all stored versions',
 	'localautosave.noContent' : 'Não há nenhum conteúdo disponível para restaurar.',
 	'localautosave.ifRestore' : 'Se você restaurar o conteúdo salvo, você vai perder todo o conteúdo que está atualmente no editor.\n\nTem certeza de que deseja restaurar o conteúdo salvo?'
 });
