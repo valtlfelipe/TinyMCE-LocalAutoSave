1bc6af47e545fca4c78cfd48e3d2d7bc31182e9d
Felipe <valtlfelipe@users.noreply.github.com>
2017-10-31 09:36:09 -0200

    Update pt-br translation

--- localautosave/langs/pt_BR.js
+++ localautosave/langs/pt_BR.js
@@ -4,5 +4,5 @@ tinyMCE.addI18n('pt_BR',{
 	'localautosave.chars' : 'caracteres',
 	'localautosave.clearAll' : 'Descartar todas as versões salvas',
 	'localautosave.noContent' : 'Não há nenhum conteúdo disponível para restaurar.',
-	'localautosave.ifRestore' : 'Se você restaurar o conteúdo salvo, você vai perder todo o conteúdo que está atualmente no editor.\n\nTem certeza de que deseja restaurar o conteúdo salvo?'
+	'localautosave.ifRestore' : 'Se você restaurar o conteúdo salvo, você irá perder todo o conteúdo que está atualmente no editor.\n\nTem certeza de que deseja restaurar o conteúdo salvo?'
 });
