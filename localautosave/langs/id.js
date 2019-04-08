054dc25821835bba39399fd442fed77373ce0985
Hasan Basri <hasanbasri1493@gmail.com>
2019-04-08 22:37:33 +0700

    Create id.js

---
+++ localautosave/langs/id.js
@@ -0,0 +1,8 @@
+tinyMCE.addI18n('id', {
+	'localautosave.restoreContent' : 'Mengembalikan konten',
+	'localautosave.chooseVersion' : 'Pilih apa yang ingin Anda pulihkan',
+	'localautosave.chars' : 'karakter',
+	'localautosave.clearAll' : 'Buang semua versi yang disimpan',
+	'localautosave.noContent' : 'Tidak ada konten yang tersedia untuk dipulihkan.',
+	'localautosave.ifRestore' : 'Jika Anda mengembalikan konten yang disimpan, Anda akan kehilangan semua konten yang saat ini ada di editor. \n\nApakah Anda yakin ingin mengembalikan konten yang disimpan?'
+});
